fac76a22c134c1ff3e19c849c7eef5d9029e2dc0
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2021-04-16 00:49:17 +0530

    img,fonts

--- src/appPage.js
+++ src/appPage.js
@@ -54,10 +54,10 @@ function logoutHandler(){
       <div className="follow" style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
       
         <h1 style={{display:"flex",width:"100%",alignItems:"center",justifyContent:"center"}}>
-        <div style={{margin:"auto",paddingLeft:"20px"}}>To-Do list</div>
+        <div style={{margin:"auto",paddingLeft:"20px",font:"50px bold"}}>To-Do list</div>
         <div style={{marginRight:"20px"}}><button onClick={logoutHandler}>Logout</button></div>
         </h1>
-        <img src="file:///C:/Users/kanna/Downloads/undraw_To_do_list_re_9nt7.svg"></img>
+        <img src="images/img.svg"></img>
       <form style={{display:"flex"}}>
         <TextField style={{paddingRight:"10px",width:"600px"}} id="standard-basic" onChange={
           (e)=>setToDo(e.target.value)} label="Add items to your list" value={toDo} />

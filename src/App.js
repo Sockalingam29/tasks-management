8d13af5c73aca966826732c42433d280f3078123
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2021-04-16 01:04:22 +0530

    login page CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import firebase from "firebase";
 import AppPage from "./appPage";
 import { useState } from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
   const[login,setLogin]=useState("");
@@ -25,8 +26,12 @@ function App() {
   }
   
   const Login=()=>(
-    <div><h1>Ready to login?</h1>
-    <button onClick={loginHandler}>Login with Google</button>
+    <div style={{display:"flex",alignItems:"center",flexDirection:"column",justifyContent:"center",height:"100%",margin:"100px"}}>
+      <h1 style={{font:"50px bold"}}>
+        <div>The To-Do App</div>
+        <img src="src/images/img.svg"/>
+        <div>Ready to login?</div></h1>
+    <Button variant="contained" onClick={loginHandler}>Login with your Google acccount</Button>
     </div>
   )
 
